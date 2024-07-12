288f73f2008fbdf88a178962fe0d85e282afd947
Derek Diniz <drk.diniz@gmail.com>
2024-07-12 12:11:10 -0300

    desafios adicionados

---
+++ scr/b1048/MultiplosDe13.java
@@ -0,0 +1,27 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class MultiplosDe13 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (i % 13 != 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+
+
+        sc.close();
+    }
+}
---
+++ scr/b1048/Pum.java
@@ -0,0 +1,30 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class Pum {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt();
+        double cont = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= 3; j++) {
+                if (j != 3) {
+                    cont = Math.pow(i,j);
+                    System.out.print((int)cont + " ");
+                }
+                else {
+                    cont = Math.pow(i,j);
+                    System.out.println((int)cont);
+                }
+            }
+        }
+
+
+        sc.close();
+    }
+}
