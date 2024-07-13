e56f67051a85f777f4e74f8fcd2862e285c05af3
Derek Diniz <drk.diniz@gmail.com>
2024-07-13 05:34:59 -0300

    desafios adicionados

---
+++ scr/b1048/Fibonacci.java
@@ -0,0 +1,10 @@
+package b1048;
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
---
+++ scr/b1048/SomandoInteirosConsecultivos.java
@@ -0,0 +1,25 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class SomandoInteirosConsecultivos {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int n = 0;
+        int sum = 0;
+        int aux = 0;
+
+        do {
+             n = sc.nextInt();
+        } while (n <= 0);
+
+        for (int i = 0; i < n; i++) {
+            aux = (i + a);
+            sum += aux;
+            aux = 0;
+        }
+        System.out.println(sum);
+    }
+}
