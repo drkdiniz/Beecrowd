f5a842d30e7285dbba32378ab88a8518525b1c3a
Derek Diniz <drk.diniz@gmail.com>
2024-07-18 14:07:45 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ scr/b1048/Matriz123.java
@@ -0,0 +1,51 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class Matriz123 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            if (n < 3 || n >= 70) break;
+
+            int[][] matriz = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i + j == n - 1) {
+                        matriz[i][j] = 2;
+                    } else if (i == j) {
+                         matriz[i][j] = 1;
+                    }else {
+                        matriz[i][j] = 3;
+                    }
+
+                }
+            }
+            for (int i = 0; i < matriz.length; i++) {
+                for (int j = 0; j < matriz[i].length; j++) {
+                    if (j == 0) {
+                        sb.append(matriz[i][j]);
+                    } else {
+                        sb.append(" ").append(matriz[i][j]);
+                    }
+
+                }
+                sb.append("\n");
+            }
+        }
+
+        System.out.print(sb);
+        sc.close();
+    }
+}
+
+
+
+
+
