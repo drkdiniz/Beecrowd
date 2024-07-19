185ae94b16c4462b5f73fbde4c8c13cae421a107
Derek Diniz <drk.diniz@gmail.com>
2024-07-19 20:08:13 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ scr/b1048/VaiTerCopa.java
@@ -0,0 +1,26 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class VaiTerCopa {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true) {
+            if (!sc.hasNextInt()) break;
+            int n = sc.nextInt();
+            if (n < 0 || n > 100) break;
+
+            if (n == 0) {
+                System.out.println("vai ter copa!");
+            }
+            else {
+                System.out.println("vai ter duas!");
+            }
+        }
+
+        sc.close();
+    }
+}
