103c3506b595fbdb3f0a1c5d1366e0042972a6a9
Derek Diniz <drk.diniz@gmail.com>
2024-07-21 05:31:27 -0300

    desafios adicionados

--- scr/b1048/MatrizQuadrada4.java
+++ scr/b1048/MatrizQuadrada4.java
@@ -17,17 +17,23 @@ public class MatrizQuadrada4 {
 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (i == (n - 1)/2 && j == (n - 1)/2) {
-                        matriz[i][j] = 4;
-                    }
-                    else if (i != 0 && i != n-1 && j != 0 & j != n-1) {
-                        matriz[i][j] = 1;
-                    }
-                    else if (i == j) {
+                    if (i == j) {
                         matriz[i][j] = 2;
                     }
                     else if (j == n - 1 - i) {
                         matriz[i][j] = 3;
+                    }
+                }
+            }
+            int inicio = n / 3;
+            int fim = n - inicio;
+            for (int i = inicio; i < fim; i++) {
+                for (int j = inicio; j < fim; j++) {
+                    if (i == (n - 1)/2 && j == (n - 1)/2) {
+                        matriz[i][j] = 4;
+                    }
+                    else {
+                        matriz[i][j] = 1;
                     }
                 }
             }
@@ -46,6 +52,9 @@ public class MatrizQuadrada4 {
         sc.close();
     }
 }
+
+
+
 
 
 
