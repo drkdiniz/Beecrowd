7c166266b641d61dd3c3f35636cfc328e2558495
Derek Diniz <drk.diniz@gmail.com>
2024-07-26 13:00:27 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ scr/b1048/ConverterParaHexadecimal.java
@@ -0,0 +1,18 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class ConverterParaHexadecimal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        String hexadecimal = Integer.toHexString(n).toUpperCase();
+
+        System.out.println(hexadecimal);
+
+        sc.close();
+    }
+}
---
+++ scr/b1048/Triangulo.java
@@ -0,0 +1,31 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class Triangulo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        boolean triangulo = testeTriangulo(a, b, c) || testeTriangulo(a, b, d) ||
+                testeTriangulo(a, c, d) || testeTriangulo(b, c, d);
+
+        if (triangulo) {
+            System.out.println("S");
+        }
+        else {
+            System.out.println("N");
+        }
+    }
+
+    private static boolean testeTriangulo(int x, int y, int z) {
+        return x + y > z && x + z > y && y + z > x;
+
+    }
+
+}
