9308cb4b52a9cb7de20cfb67a7c7ae6fc4e84ff9
Derek Diniz <drk.diniz@gmail.com>
2024-07-29 19:40:57 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ scr/b1048/PedraPapelAtaqueAereo.java
@@ -0,0 +1,54 @@
+package b1048;
+
+import java.util.Scanner;
+
+public class PedraPapelAtaqueAereo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String jogador1 = sc.next().toLowerCase();
+            String jogador2 = sc.next().toLowerCase();
+            switch (jogador1) {
+                case "ataque":
+                    if (jogador2.equals("ataque")) {
+                        System.out.println("Aniquilacao mutua");
+                    }
+                    else if (jogador2.equals("pedra")) {
+                        System.out.println("Jogador 1 venceu");
+                    }
+                    else if (jogador2.equals("papel")) {
+                        System.out.println("Jogador 1 venceu");
+                    }
+                    break;
+                case "pedra":
+                    if (jogador2.equals("ataque")) {
+                        System.out.println("Jogador 2 venceu");
+                    }
+                    else if (jogador2.equals("pedra")) {
+                        System.out.println("Sem ganhador");
+                    }
+                    else if (jogador2.equals("papel")) {
+                        System.out.println("Jogador 1 venceu");
+                    }
+                    break;
+                case "papel":
+                    if (jogador2.equals("ataque")) {
+                        System.out.println("Jogador 2 venceu");
+                    }
+                    else if (jogador2.equals("pedra")) {
+                        System.out.println("Jogador 2 venceu");
+                    }
+                    else if (jogador2.equals("papel")) {
+                        System.out.println("Ambos venceram");
+                    }
+                    break;
+            }
+
+        }
+        sc.close();
+    }
+}
